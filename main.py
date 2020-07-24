736774620998ddf30526093c6fb3d1aa26441756
cheny <chenyprivate@vip.qq.com>
2020-07-24 13:20:03 +0800

    add window notifications to notify user connect state

--- main.py
+++ main.py
@@ -40,8 +40,8 @@ def getip():
     pass
 def loggin():
     "log in to school wifi"
-    usr_name=',0,1998004'#replace 'account'with your own account
-    usr_pwd='123123'#set password here
+    usr_name=',0,'#replace 'account'with your own account
+    usr_pwd=''#set password here
     ip=getip()
     mac=get_mac()
 
