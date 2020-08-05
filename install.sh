b489435de395e2e92c45407cf425eaaa4f186eaf
cheny <chenyprivate@vip.qq.com>
2020-08-05 08:59:14 +0800

    fix installation folder

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@
 echo"正在打包程序..."
 pyinstaller -F main.py -p ./venv/lib/python3.7/site-packages
 echo "正在移动文件..."
-sudo mkdir /opt/apps/easyconnect
-sudo cp ./dist/main /opt/apps/easyconnect/easyconnect
-sudo cp EasyConnect.ico /opt/apps/easyconnect
-sudo chmod 777 /opt/apps/easyconnect/easyconnect
+sudo mkdir /opt/easyconnect
+sudo cp ./dist/main /opt/easyconnect/easyconnect
+sudo cp EasyConnect.ico /opt/easyconnect
+sudo chmod 777 /opt/easyconnect/easyconnect
 sudo cp easyconnect.desktop /usr/share/applications/easyconnect.desktop
 echo "安装完成"
 
--- uninstall.sh
+++ uninstall.sh
@@ -4,5 +4,5 @@
 #作者：cheny
 echo "正在移除程序..."
 sudo rm /usr/share/applications/easyconnect.desktop
-sudo rm /opt/apps/easyconnect -r
+sudo rm /opt/easyconnect -r
 echo "删除完成"
